f3b53fa9782e2031d43d78bca2a08ea8db4d9c30
qui2cheby <qui2cheby@gmail.com>
2024-09-12 08:02:16 -0600

    init

--- src/main/java/org/unosquare/UnosquareApplication.java
+++ src/main/java/org/unosquare/UnosquareApplication.java
@@ -3,7 +3,7 @@ package org.unosquare;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "org.unosquare")
 public class UnosquareApplication {
 
     public static void main(String[] args) {
