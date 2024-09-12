e73548f7a11d11b58334129b548430556f2c9be0
qui2cheby <qui2cheby@gmail.com>
2024-09-12 07:59:26 -0600

    adding files

---
+++ src/main/java/org/unosquare/UnosquareApplication.java
@@ -0,0 +1,13 @@
+package org.unosquare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnosquareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnosquareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/unosquare/UnosquareApplicationTests.java
@@ -0,0 +1,13 @@
+package org.unosquare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnosquareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
